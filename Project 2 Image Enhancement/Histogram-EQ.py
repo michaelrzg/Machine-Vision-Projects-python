1a38a4f038b3768cb57e447026cb53350f53f321
michaelrzg <michaelrizig@gmail.com>
2024-06-12 15:15:49 -0400

    added output for equalization

--- Project 2 Image Enhancement/Histogram-EQ.py
+++ Project 2 Image Enhancement/Histogram-EQ.py
@@ -7,26 +7,27 @@
 import matplotlib.pyplot as plottool
 import cv2
 from skimage import io
-factor  = 1/(210-140)
+
 def equalizationFunc(pixel):
 
-    return 255 * ((pixel-150)/70)
+    return 255 * (pixel/50)
 
-mapPath = 'input/sat_map.png'
-satmap = io.imread(mapPath)
+mapPath = 'input/university.png'
+university = io.imread(mapPath)
+university = cv2.cvtColor(university,cv2.COLOR_BGR2RGB)
 greyrange = [0,256]
 maxsize= [256]
-hist = cv2.calcHist([satmap], [0], None, maxsize, greyrange)
-out= satmap.copy()
+hist = cv2.calcHist([university], [0], None, maxsize, greyrange)
+out= university.copy()
 plottool.imshow(out)
 plottool.show()
 plottool.plot(hist)
 plottool.show()
 factor  = 256/64
 print(out.shape)
-for k in range(0,satmap.shape[0]):
-    for p in range(0,satmap.shape[1]):
-        out[k][p] = equalizationFunc(satmap[k][p])
+for k in range(0,university.shape[0]):
+    for p in range(0,university.shape[1]):
+        out[k][p] = equalizationFunc(university[k][p])
     
         
 
@@ -37,3 +38,4 @@ plottool.show()
 
 plottool.imshow(out)
 plottool.show()
+cv2.imwrite('output/hist/university/uniEqualized.png',out)
